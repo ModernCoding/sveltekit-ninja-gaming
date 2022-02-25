b9a740772a53f556c774721bbad68688153fed35
Cormorant Programming <cormorant.programming@gmail.com>
2022-02-25 07:59:52 +0700

    layout

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,14 +1,3 @@
-<script>
-
-  import Title from '$lib/title.svelte'
-
-  let title = "Ninja Gaming Guides"
-
-</script>
-
-
-<Title title={ title } />
-
 <div class="about">
   <h2>About Us</h2>
   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt laborum magnam deleniti recusandae, quis aperiam, architecto neque, voluptatem dicta expedita aut id voluptate omnis consectetur dolore! Quo quae perferendis vel dignissimos quasi illum aperiam dolores, id, laudantium voluptatibus fugiat accusantium impedit velit modi ratione iure qui culpa! Repellendus cupiditate aliquid quam magni. Laudantium, laboriosam quibusdam. Provident, quae incidunt! Aspernatur, vel?</p>
